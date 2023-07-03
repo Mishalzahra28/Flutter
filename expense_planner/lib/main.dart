37fd93111ae599be5181ac6a59a6c37b3c454264
Mishal Zahra <mishalzahra281@gmail.com>
2023-07-03 15:15:44 +0500

    adding transactions

--- expense_planner/lib/main.dart
+++ expense_planner/lib/main.dart
@@ -1,93 +1,91 @@
 import 'package:flutter/material.dart';
-
-import './result.dart';
-import './quiz.dart';
+import "./transaction.dart";
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  var _Index = 0;
-  var _totalScore = 0;
-
-  void chooseAnswer(int score) {
-    _totalScore = _totalScore + score;
-    setState(() {
-      _Index = _Index + 1;
-    });
-  }
-
-  void resetQuiz() {
-    setState(() {
-      _Index = 0;
-      _totalScore = 0;
-    });
-  }
-
-  var questions = [
-    {
-      "question": "What's Capital of Pakistan?",
-      "answer": [
-        {"text": "Islamabad", "score": 1},
-        {"text": "Lahore", "score": 0},
-        {"text": "Karachi", "score": 0}
-      ]
-    },
-    {
-      "question": "What's Capital Of Balochistan?",
-      "answer": [
-        {"text": "Sibbi", "score": 0},
-        {"text": "Quetta", "score": 1},
-        {"text": "Gawadar", "score": 0}
-      ]
-    },
-    {
-      "question": "What's Capital Of Sindh?",
-      "answer": [
-        {"text": "Hayderabad", "score": 0},
-        {"text": "Larkana", "score": 0},
-        {"text": "Karachi", "score": 1}
-      ]
-    },
-    {
-      "question": "What's Capital Of Punjab?",
-      "answer": [
-        {"text": "Islamabad", "score": 0},
-        {"text": "Lahore", "score": 1},
-        {"text": "Rawalpindi", "score": 0}
-      ]
-    },
-    {
-      "question": "What's Capital Of KPK?",
-      "answer": [
-        {"text": "Peshawar", "score": 1},
-        {"text": "Mardan", "score": 0},
-        {"text": "DI Khan", "score": 0}
-      ]
-    },
-  ];
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('First App'),
-            ),
-            body: _Index < questions.length
-                ? Quiz(
-                    questions: questions,
-                    chooseAnswer: chooseAnswer,
-                    Index: _Index)
-                : Result(
-                    finalScore: _totalScore,
-                    reset: resetQuiz,
-                  )));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: "t2",
+        title: "Weekly Groceries",
+        amount: 16.53,
+        date: DateTime.now()),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Planner"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        children: [
+          Card(
+              child: Container(
+            width: double.infinity,
+            child: Text("Chart"),
+          )),
+          Column(
+            children: transaction.map((txt) {
+              return Card(
+                  child: Row(
+                children: [
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 1)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          "\$${txt.amount}",
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                              fontSize: 20),
+                        ),
+                      )),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        txt.title,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 17),
+                      ),
+                      Text(
+                        txt.date.toString(),
+                        style: TextStyle(color: Colors.grey, fontSize: 14),
+                      )
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          )
+        ],
+      ),
+    );
   }
 }
