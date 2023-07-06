af4632f445dc966168e70b29b5f9068aa33e0a7d
Mishal Zahra <mishalzahra281@gmail.com>
2023-07-06 12:59:56 +0500

    adding date picker

--- expense_planner/lib/widgets/new_transactions.dart
+++ expense_planner/lib/widgets/new_transactions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addTransaction;
@@ -10,8 +11,8 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
-
   final amountController = TextEditingController();
+  DateTime selectedDate = DateTime.now();
 
   void submitHandler() {
     final enteredTitle = titleController.text;
@@ -23,6 +24,22 @@ class _NewTransactionState extends State<NewTransaction> {
     Navigator.of(context).pop();
   }
 
+  void displayDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2018),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) => {
+          if (pickedDate == null)
+            {
+              // return
+            }
+          else
+            {selectedDate = pickedDate}
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -30,6 +47,7 @@ class _NewTransactionState extends State<NewTransaction> {
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             TextField(
               controller: titleController,
@@ -42,12 +60,29 @@ class _NewTransactionState extends State<NewTransaction> {
               keyboardType: TextInputType.number,
               onSubmitted: (_) => submitHandler(),
             ),
-            TextButton(
-                onPressed: submitHandler,
-                child: const Text(
-                  "Add Transaction",
-                  style: TextStyle(color: Colors.purple, fontSize: 15),
-                ))
+            Row(
+              children: [
+                TextButton(
+                    onPressed: displayDatePicker, child: Text("Choose Date")),
+                Text(selectedDate == null
+                    ? "No Date Chosen"
+                    : DateFormat.yMd(selectedDate).toString())
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple,
+                    ),
+                    onPressed: submitHandler,
+                    child: const Text(
+                      "Add Transaction",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    )),
+              ],
+            )
           ],
         ),
       ),
